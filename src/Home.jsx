521ae58850a0b2f7e1fd43e8f9007a5b3ec0c973
sohamhazra2000 <sohamhazra15@gmail.com>
2024-03-14 21:21:24 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,130 @@
+import { Button, Container, Grid, Paper, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import React from 'react'
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+      const onSubmit = (data) => {
+        const apiUrl = "https://firstapi-5cqk.onrender.com/a";
+    
+        const UpData = {
+            "empid": data.empid,
+            "name": data.name,
+            "designation": data.designation,
+            "adress": data.adress,
+            "phoneno":data.phoneno,
+           "email":data.email,
+
+        };
+        axios
+          .post(apiUrl, UpData).then((res)=>{alert(res)})
+          
+      };
+      return (
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6} sx={{ margin: "0 auto",marginTop:"50px",marginBottom:"250px" }}>
+              <Paper elevation={3} sx={{ padding: 2 }}>
+                <Typography variant="h5" gutterBottom>
+                  Register from
+                </Typography>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <TextField
+                    {...register("empid", { required: true, maxLength: 20 })}
+                    label="empid"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={errors.empid}
+                    helperText={errors.empid && "id is required"}
+                  />
+                  <TextField
+                    {...register("name", { required: true, maxLength: 200})}
+                    label="name"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={!!errors.name}
+                    helperText={errors.name && "name is required"}
+                  />
+                  
+                  <TextField
+                    {...register("designation", { required: true })}
+                    label="designation"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={!!errors.designation}
+                    helperText={errors.desgantion && "desigantion is required"}
+                  />
+                  <TextField
+                    {...register("adress", { required: true })}
+                    label="adress"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={!!errors.adress}
+                    helperText={errors.adress && "adress is required"}
+                  />
+                   <TextField
+                    {...register("phoneno", { required: true, maxLength: 12 })}
+                    label="phoneno"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={errors.phoneno}
+                    helperText={errors.phoneno && "phone is required"}
+                  />
+                  <TextField
+                    {...register("email", {
+                      required: true,
+                      pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                      
+                    })}
+                    label="Your Email"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={!!errors.email}
+                    helperText={errors.email && errors.email.type === "required"  && "Email is required"||  errors.email && errors.email.type === "pattern" &&"valid email is required" }
+                  />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    onClick={handleSubmit(onSubmit)}
+                    sx={{ marginTop: 2 ,marginBottom:6}}
+                  >
+                    Register
+                  </Button>
+                  <Typography variant="h5" gutterBottom>
+                  for serch click below
+                </Typography>
+                  <Link to="/serch">
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    sx={{ marginTop: 2 }}
+                  >
+                  serch
+                  </Button>
+                  </Link>
+                  
+                </form>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      );
+}
---
+++ src/Search.jsx
@@ -0,0 +1,94 @@
+import { Button, Card, CardContent, Container, Grid, Paper, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+export default function Search() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+      const[A,setA]=useState({empid:"",name:"",designation:"",adress:"",phoneno:"",email:""})
+      const onSubmit = (data) => {
+        const apiUrl = "https://firstapi-5cqk.onrender.com/aa";
+    
+        const UpData = {
+            "id": data.empid,
+
+        };
+        axios
+          .post(apiUrl, UpData).then(son=>setA(son.data[0]))
+          
+      };
+      console.log(A.empid +A.name+A.designation)
+      return (
+        <Container>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6} sx={{ margin: "0 auto",marginTop:"150px" }}>
+              <Paper elevation={3} sx={{ padding: 2 }}>
+                <Typography variant="h5" gutterBottom>
+                  Register from
+                </Typography>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <TextField
+                    {...register("empid", { required: true, maxLength: 20 })}
+                    label="empid"
+                    fullWidth
+                    margin="normal"
+                    variant="outlined"
+                    error={errors.empid}
+                    helperText={errors.empid && "id is required"}
+                  />
+                  
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    onClick={handleSubmit(onSubmit)}
+                    sx={{ marginTop: 2 ,marginBottom:6}}
+                  >
+                    serch
+                  </Button>
+                  <Typography variant="h5" gutterBottom>
+                  Do have an acoount then login below
+                </Typography>
+                  
+                </form>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={6} sx={{ margin: "0 auto",marginTop:"150px" }}>
+            <Card sx={{ Width: 345,height:314 }}>
+      
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          empid: {A.empid}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+         name: {A.name}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          designation: {A.designation}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          adress: {A.adress}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          phone: {A.phoneno}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          email: {A.email}
+        </Typography>
+      </CardContent>
+    
+    </Card>
+    </Grid>
+          </Grid>
+       
+        </Container>
+      );
+}
+
